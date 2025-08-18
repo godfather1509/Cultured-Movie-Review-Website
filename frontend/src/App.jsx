8775cf5b0e8ab7c48572491ef6c00ab5a8560441
Ayush <ayushg1500@gmail.com>
2025-08-18 17:58:05 +0530

    Added New components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import api from './api/apiConfig'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import About from './components/About'
+import LogIn from './components/Login'
+import Register from './components/Register'
 import NotFound from './components/NotFound'
 
 function App() {
@@ -28,6 +31,27 @@ function App() {
         element: <>
           <Navbar />
           <Home movies={movies} />
+        </>
+      },
+      {
+        path: "/about",
+        element: <>
+          <Navbar />
+          <About/>
+        </>
+      },
+      {
+        path: "/login",
+        element: <>
+          <Navbar />
+          <LogIn/>
+        </>
+      },
+      {
+        path: "/register",
+        element: <>
+          <Navbar />
+          <Register/>
         </>
       },
       {
